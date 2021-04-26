7f60c47f8b2cec7297b7c7fc1fe7de95a9717a4a
Oleg <odasheyev@mail.ru>
2021-04-26 23:15:10 +0800

    Fixed classes

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -2,18 +2,16 @@ package ru.netology.manager;
 
 import ru.netology.domain.Afisha;
 
-public class PosterManager {
+public class AfishaManager {
+    int amountFilmsMax;
     private Afisha[] items = new Afisha[0];
 
     public void add(Afisha item) {
         // создаём новый массив размером на единицу больше
         int length = items.length + 1;
         Afisha[] tmp = new Afisha[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
+
+
         System.arraycopy(items, 0, tmp, 0, items.length);
         // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
@@ -22,28 +20,17 @@ public class PosterManager {
     }
 
     public Afisha[] getAll() {
-        Afisha[] result = new Afisha[items.length];
+        Afisha[] result = new Afisha[amountFilmsMax];
         // перебираем массив в прямом порядке
         // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
+            int index = amountFilmsMax - i - 1;
             result[i] = items[index];
         }
         return result;
     }
 
-    // наивная реализация
-    public void removeById(int id) {
-        int length = items.length - 1;
-        Afisha[] tmp = new Afisha[length];
-        int index = 0;
-        for (Afisha item : items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
-            }
-        }
-        // меняем наши элементы
-        items = tmp;
+    public AfishaManager(int amountFilmsMax) {
+        this.amountFilmsMax = amountFilmsMax;
     }
 }
